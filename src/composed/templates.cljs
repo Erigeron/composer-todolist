8ead544b8096258f588bff9a1f914c6308d8d61e
jiyinyiyong <jiyinyiyong@gmail.com>
2019-02-25 01:58:26 +0800

    some initial works on UI

--- src/composed/templates.cljs
+++ src/composed/templates.cljs
@@ -35,4 +35,104 @@
    }
   }
  }
+ "footer" {
+  :id "system", :type :box, :layout :row
+  :props {}
+  :attrs {}
+  :presets #{}
+  :style {"background-color" "hsl(0,0%,100%)", "padding" "8px"}
+  :children {
+   "T" {
+    :id "T", :type :button, :layout nil
+    :props {"text" "Clear"}
+    :attrs {}
+    :presets #{}
+    :style {}
+    :children {}
+   }
+   "j" {
+    :id "j", :type :space, :layout nil
+    :props {"width" "8"}
+    :attrs {}
+    :presets #{}
+    :style {}
+    :children {}
+   }
+  }
+ }
+ "header" {
+  :id "system", :type :box, :layout :row-middle
+  :props {}
+  :attrs {}
+  :presets #{}
+  :style {"background-color" "hsl(0,0%,100%)", "padding" "8px"}
+  :children {
+   "T" {
+    :id "T", :type :text, :layout nil
+    :props {"value" "Todolist demo"}
+    :attrs {}
+    :presets #{:font-fancy}
+    :style {}
+    :children {}
+   }
+   "b" {
+    :id "b", :type :space, :layout nil
+    :props {"width" "8"}
+    :attrs {}
+    :presets #{}
+    :style {}
+    :children {}
+   }
+   "j" {
+    :id "j", :type :input, :layout nil
+    :props {}
+    :attrs {"placeholder" "Todo thing"}
+    :presets #{}
+    :style {}
+    :children {}
+   }
+   "n" {
+    :id "n", :type :space, :layout nil
+    :props {"width" "8"}
+    :attrs {}
+    :presets #{}
+    :style {}
+    :children {}
+   }
+   "r" {
+    :id "r", :type :button, :layout nil
+    :props {}
+    :attrs {}
+    :presets #{}
+    :style {}
+    :children {}
+   }
+  }
+ }
+ "records" {
+  :id "system", :type :list, :layout :row
+  :props {}
+  :attrs {}
+  :presets #{}
+  :style {}
+  :children {
+   "T" {
+    :id "T", :type :box, :layout nil
+    :props {}
+    :attrs {}
+    :presets #{}
+    :style {}
+    :children {
+     "T" {
+      :id "T", :type :box, :layout nil
+      :props {}
+      :attrs {}
+      :presets #{}
+      :style {}
+      :children {}
+     }
+    }
+   }
+  }
+ }
 })
